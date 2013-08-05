2ab68f4292046cf0dc0cf6b836abb24ddd36f9ea
Balogh Gergõ <geryxyz@inf.u-szeged.hu>
2013-08-05 17:49:49 +0200

    description added

--- AKindnessCommons/Properties/AssemblyInfo.cs
+++ AKindnessCommons/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AKindnessCommons")]
+[assembly: AssemblyDescription("Some basic kindness for .Net or C#.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gergő Balogh")]
 [assembly: AssemblyProduct("AKindnessCommons")]
